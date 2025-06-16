ed50cddefa3fa97dc1929caa97e6d704d147b59e
Davi <davirdavanzo@gmail.com>
2025-06-16 10:06:01 -0300

    #97518 Remoção das bordas no tema escuro

--- main.js
+++ main.js
@@ -235,14 +235,16 @@ function applyTheme(theme = "default") {
             '--second-color': "rgb(225, 225, 225)",
             '--font-color': "rgb(74, 74, 74)",
             '--hover-color': "rgb(195, 195, 195)",
-            '--border-color': "rgb(203, 203, 203)"
+            '--border-color': "rgb(203, 203, 203)",
+            "--border-style" : "solid"
         },
         dark: {
             '--main-color': "rgb(24, 26, 27)",
             '--second-color': "rgb(44, 47, 49)",
             '--font-color': "rgb(210, 210, 210)",
             '--hover-color': "rgb(70, 75, 78)",
-            '--border-color': "rgb(60, 64, 66)"
+            '--border-color': "rgb(60, 64, 66)",
+            "--border-style" : "none"
         }
     };
 
@@ -251,7 +253,8 @@ function applyTheme(theme = "default") {
         "--second-color",
         "--font-color",
         "--hover-color",
-        "--border-color"
+        "--border-color",
+        "--border-style"
     ]
 
     themeVariables.forEach(variable => {
