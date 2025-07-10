49068e99ad694dc131121dae5f0717e938d8efc7
Davi <davirdavanzo@gmail.com>
2025-07-10 10:53:14 -0300

    #98758 - Organização do código

--- api/Modules/Registrations/IRegistrationService.cs
+++ api/Modules/Registrations/IRegistrationService.cs
@@ -1,12 +1,13 @@
-namespace Api.Modules.Registrations;
-
-public interface IRegistrationService
+namespace Api.Modules.Registrations
 {
-    void CreateRegistration(RegistrationDto registration);
-    RegistrationDto GetSingleRegistration(Guid id);
-    RegistrationsPageDto GetRegistrationsPage(int start, int increment, string sortKey, bool descending, string query);
-    List<RegistrationDto> GetAllRegistrations();
-    void UpdateRegistration(RegistrationDto registration);
-    void DeleteRegistration(Guid id);
+    public interface IRegistrationService
+    {
+        void CreateRegistration(RegistrationDto registration);
+        RegistrationDto GetSingleRegistration(Guid id);
+        RegistrationsPageDto GetRegistrationsPage(int start, int increment, string sortKey, bool descending, string query);
+        List<RegistrationDto> GetAllRegistrations();
+        void UpdateRegistration(RegistrationDto registration);
+        void DeleteRegistration(Guid id);
 
+    }
 }
