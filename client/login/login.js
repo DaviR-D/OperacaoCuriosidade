d3092895754819e6f446b1d0a06c18fd234bc3bb
Davi <davirdavanzo@gmail.com>
2025-07-16 14:49:50 -0300

    #98758 - Extração da função de tema

--- client/login/login.js
+++ client/login/login.js
@@ -69,36 +69,3 @@ function parseToken(token){
   return JSON.parse(decodedPayload);
 }
 
-function applyTheme(theme = "default") {
-  let themeColors = {
-    default: {
-      '--main-color': "white",
-      '--second-color': "rgb(225, 225, 225)",
-      '--font-color': "rgb(74, 74, 74)",
-      '--highlight-color': "rgb(195, 195, 195)",
-      '--border-color': "rgb(203, 203, 203)"
-    },
-    dark: {
-      '--main-color': "rgb(24, 26, 27)",
-      '--second-color': "rgb(44, 47, 49)",
-      '--font-color': "rgb(210, 210, 210)",
-      '--highlight-color': "rgb(70, 75, 78)",
-      '--border-color': "rgb(60, 64, 66)"
-    }
-  };
-
-  let themeVariables = [
-    "--main-color",
-    "--second-color",
-    "--font-color",
-    "--highlight-color",
-    "--border-color"
-  ]
-
-  themeVariables.forEach(variable => {
-    document.documentElement.style.setProperty(variable, themeColors[theme][variable]);
-  });
-
-  localStorage.setItem("theme", theme);
-}
-
