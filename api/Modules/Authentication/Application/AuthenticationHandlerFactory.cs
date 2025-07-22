ed07b63de07ba25642633d5ec93b73be10b0d56a
Davi <davirdavanzo@gmail.com>
2025-07-22 10:53:05 -0300

    #100216 - Factory para criação de handlers na autenticação

---
+++ api/Modules/Authentication/Application/AuthenticationHandlerFactory.cs
@@ -0,0 +1,25 @@
+﻿using Api.Modules.Authentication.Application.Commands.Authenticate;
+using Api.Modules.Authentication.Application.Commands.CreateUser;
+using Api.Shared.Interfaces;
+
+namespace Api.Modules.Authentication.Application
+{
+    public class AuthenticationHandlerFactory(IServiceProvider service)
+    {
+        public IRequestHandler<IRequestOutput, IRequestInput> GetHandler(string endpoint)
+        {
+            IRequestHandler<IRequestOutput, IRequestInput> handler;
+
+            if (endpoint == "/api/authentication/signup")
+            {
+                handler = service.GetService<CreateUserHandler>();
+                return handler;
+            } else if (endpoint == "/api/authentication/")
+            {
+                handler = service.GetService<AuthenticateHandler>();
+                return handler;
+            }
+            return null;
+        }
+    }
+}
