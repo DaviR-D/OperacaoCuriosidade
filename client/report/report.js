7c1331dd55717a67a3f50d4221fee4bf7f2253dd
Davi <davirdavanzo@gmail.com>
2025-07-25 08:44:26 -0300

    #100216 - Reaproveitamento de código repetido

--- client/report/report.js
+++ client/report/report.js
@@ -30,27 +30,6 @@ async function printTable() {
     document.body.classList.remove("blur");
 }
 
-function setTableSettings(){
-    main.tableHeader = [`
-        <tr id="tableHeader">
-            <th class="column" onclick="sortTable('name')">Nome ${main.arrow.name ? main.arrow.name : ""}</th>
-            <th class="column" onclick="sortTable('email')">Email ${main.arrow.email ? main.arrow.email : ""}</th>
-            <th class="column" onclick="sortTable('status')">Status ${main.arrow.status ? main.arrow.status : ""}</th>
-            <th class="column" onclick="sortTable('date')">Data ${main.arrow.date ? main.arrow.date : ""}</th>
-        </tr>`
-    ];
-    main.tableContent = (register) => {
-        return `
-            <tr>
-                <td>${register.name}</td>
-                <td>${register.email}</td>
-                <td><span style="border-radius:5px; padding:5px;" class=${register.status == "Ativo" ? "active" : "inactive"}>${register.status}</span></td>
-                <td>${new Date(register.date).toLocaleDateString('pt-BR')}</td>
-                <td class="actions" style="display: none;">
-                    <button class="editButton material-symbols-outlined" onclick="editClient('${register.id}')">edit</button>
-                    <button class="deleteButton material-symbols-outlined" onclick="showDeleteConfirmation('${register.id}')">delete</button>
-                </td>
-            </tr>
-            `}
-    main.getTablePage = getClients;
+function setTableSettings() {
+    loadClientsTable();
 }
