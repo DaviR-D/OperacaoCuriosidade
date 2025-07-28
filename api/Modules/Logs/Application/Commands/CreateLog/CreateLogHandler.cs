b558c56c9fcd7b9421f6d6c09e85a3deac4001cf
Davi <davirdavanzo@gmail.com>
2025-07-28 10:35:40 -0300

    #100216 - Correção de erro de concorrência na criação dos logs

--- api/Modules/Logs/Application/Commands/CreateLog/CreateLogHandler.cs
+++ api/Modules/Logs/Application/Commands/CreateLog/CreateLogHandler.cs
@@ -6,13 +6,17 @@ namespace Api.Modules.Logs.Application.Commands.CreateLog
 {
     public class CreateLogHandler(LogRepository repository) : IRequestHandler<IRequestOutput, IRequestInput>
     {
+        private static readonly Lock _lock = new();
         public IRequestOutput Handle(IRequestInput input)
         {
             var command = (CreateLogCommand)input;
             command.Log.Id = Guid.NewGuid();
             command.Log.UserId = command.UserId;
             command.Log.TimeStamp = DateTime.Now;
-            repository.Create(LogDtoMapper.ToEntity(command.Log));
+            lock (_lock)
+            {
+                repository.Create(LogDtoMapper.ToEntity(command.Log));
+            }
             return new CreateLogResponse();
         }
     }
