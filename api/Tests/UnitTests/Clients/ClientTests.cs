d31bfcbc388899e418b7470336893c08906c1bae
Davi <davirdavanzo@gmail.com>
2025-07-31 11:45:28 -0300

    #100216 - Teste de campo vazio na criação

--- api/Tests/UnitTests/Clients/ClientTests.cs
+++ api/Tests/UnitTests/Clients/ClientTests.cs
@@ -124,5 +124,32 @@ namespace Api.Tests.UnitTests.Clients
             //Assert
             Assert.IsType<ConflictObjectResult>(result);
         }
+
+        [Fact]
+        public void Create_Client_With_Empty_Field_Test()
+        {
+            //Arrange
+            ClientMockDependencies mockDependencies = new ClientMockDependencies();
+            ClientController controller = mockDependencies.ServiceProvider.GetService<ClientController>();
+
+            ClientDto clientMock = new(
+                name: "Maria Clara",
+                email: "fernando.lima@example.com",
+                status: "",
+                pending: true,
+                age: 28,
+                address: "Rua das Flores, 123",
+                other: "Artista plástica com exposições em galerias locais.",
+                interests: "Artes, Pintura",
+                feelings: "Reflexiva",
+                values: "Autenticidade, Beleza"
+            );
+
+            //Act
+            var result = controller.Create(clientMock);
+
+            //Assert
+            Assert.IsType<UnprocessableEntityObjectResult>(result);
+        }
     }
 }
