5d755f02a823e7c86334043e14b07d8d518ef32a
Davi <davirdavanzo@gmail.com>
2025-08-04 08:49:45 -0300

    #100216 - Remoção de atributo não usado

--- api/Modules/Clients/Application/Commands/UnlockClient/UnlockClientResponse.cs
+++ api/Modules/Clients/Application/Commands/UnlockClient/UnlockClientResponse.cs
@@ -2,9 +2,8 @@
 
 namespace Api.Modules.Clients.Application.Commands.UnlockClient
 {
-    public class UnlockClientResponse(string? newToken = null, string? message = null) : IRequestOutput
+    public class UnlockClientResponse(string? message = null) : IRequestOutput
     {
         public string? Message { get; set; } = message;
-        public string? NewToken { get; set; } = newToken;
     }
 }
