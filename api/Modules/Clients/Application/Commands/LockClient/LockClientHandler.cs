20fa6c65a2e06ad8c53f1d505a996d8df7f2582a
Davi <davirdavanzo@gmail.com>
2025-08-05 13:14:19 -0300

    #100216 - Aumento de validade do token de edição

--- api/Modules/Clients/Application/Commands/LockClient/LockClientHandler.cs
+++ api/Modules/Clients/Application/Commands/LockClient/LockClientHandler.cs
@@ -26,7 +26,7 @@ namespace Api.Modules.Clients.Application.Commands.LockClient
                 if (client.Lock != null && client.Lock > DateTime.UtcNow)
                     return new LockClientResponse(message: "client already locked");
 
-                var expireTime = DateTime.UtcNow.AddSeconds(15);
+                var expireTime = DateTime.UtcNow.AddSeconds(60);
 
                 client.Lock = expireTime;
                 var token = GenerateToken(command.UserId, command.ClientId, expireTime);
