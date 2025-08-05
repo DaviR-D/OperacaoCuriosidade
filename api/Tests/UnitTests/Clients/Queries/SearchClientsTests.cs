a3dd1fcc7c130b3ba5115b7fc5a4ee7fac399d07
Davi <davirdavanzo@gmail.com>
2025-08-05 08:46:13 -0300

    #100216 - Criação dos últimos testes do CRUD

---
+++ api/Tests/UnitTests/Clients/Queries/SearchClientsTests.cs
@@ -0,0 +1,84 @@
+﻿using Api.Modules.Clients.Domain;
+using Api.Modules.Clients.Presentation;
+using Microsoft.AspNetCore.Mvc;
+using Xunit;
+
+namespace Api.Tests.UnitTests.Clients.Queries
+{
+    public class SearchClientsTests
+    {
+        [Fact]
+        public void Search_Clients_With_Filled_List_Test()
+        {
+            //Arrange
+            ClientMockDependencies mockDependencies = new();
+            ClientController controller = mockDependencies.ServiceProvider.GetService<ClientController>();
+            Guid clientMockId = Guid.NewGuid();
+            Client clientMock = new(
+                id: clientMockId,
+                name: "Maria Clara",
+                email: "maria.clara@example.com",
+                status: "Inactive",
+                pending: true,
+                date: DateTime.Now,
+                age: 28,
+                address: "Rua das Flores, 123",
+                other: "Artista plástica com exposições em galerias locais.",
+                interests: "Artes, Pintura",
+                feelings: "Reflexiva",
+                values: "Autenticidade, Beleza"
+            );
+            mockDependencies.ClientsMock.Add(clientMock);
+
+            //Act
+            var result = controller.SearchClients(start: 0, increment: 10, query: "test");
+
+            //Assert
+            Assert.IsType<OkObjectResult>(result);
+        }
+
+        [Fact]
+        public void Search_Clients_With_Empty_List_Test()
+        {
+            //Arrange
+            ClientMockDependencies mockDependencies = new();
+            ClientController controller = mockDependencies.ServiceProvider.GetService<ClientController>();
+
+            //Act
+            var result = controller.SearchClients(start: 0, increment: 10, query: "test");
+
+            //Assert
+            Assert.IsType<OkObjectResult>(result);
+        }
+
+        [Fact]
+        public void Search_Clients_Without_Query_Test()
+        {
+            //Arrange
+            ClientMockDependencies mockDependencies = new();
+            ClientController controller = mockDependencies.ServiceProvider.GetService<ClientController>();
+            Guid clientMockId = Guid.NewGuid();
+            Client clientMock = new(
+                id: clientMockId,
+                name: "Maria Clara",
+                email: "maria.clara@example.com",
+                status: "Inactive",
+                pending: true,
+                date: DateTime.Now,
+                age: 28,
+                address: "Rua das Flores, 123",
+                other: "Artista plástica com exposições em galerias locais.",
+                interests: "Artes, Pintura",
+                feelings: "Reflexiva",
+                values: "Autenticidade, Beleza"
+            );
+            mockDependencies.ClientsMock.Add(clientMock);
+
+            //Act
+            var result = controller.SearchClients(start: 0, increment: 10);
+
+            //Assert
+            Assert.IsType<OkObjectResult>(result);
+        }
+    }
+}
