1cf2bf6ae54e7e928ccc1a9ae390e89a6112b653
Davi <davirdavanzo@gmail.com>
2025-08-08 14:56:56 -0300

    #100216 - Correção de validação

--- Test/obj/Debug/net9.0/Test.AssemblyInfo.cs
+++ Test/obj/Debug/net9.0/Test.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10307ccef905e0c9871676be49ea977a2880651d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d09a7009fc6c77b3aeadc1ef56e2eb597488a08")]
 [assembly: System.Reflection.AssemblyProductAttribute("Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
