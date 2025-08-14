2b86420f5e1d64e391028294cab561f1f85162f0
Davi <davirdavanzo@gmail.com>
2025-08-14 11:15:16 -0300

    #102626 - Criação de DbContext

---
+++ api/Shared/DB/ApiDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Api.Modules.Authentication.Domain;
+using Api.Modules.Clients.Domain;
+using Api.Modules.Logs.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Shared.DB
+{
+    public class ApiDbContext : DbContext
+    { 
+        public DbSet<User> Users {  get; set; }
+        //public DbSet<Client> Clients { get; set; }
+        //public DbSet<Log> Logs { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer("server=FORLOGIC762;Database=ApiDb;Trusted_Connection=True");
+        }
+    }
+}
